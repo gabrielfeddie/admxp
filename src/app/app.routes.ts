2b41430cf447b9ba9746a6b5248e053aa3115504
gabrielfeddie <gabrielfelix099@hotmail.com>
2025-11-28 06:53:36 -0300

    master: Criação do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Receitas } from './receitas/receitas';
+import { PedidoDeLogin } from './pedido-de-login/pedido-de-login';
+import { CHomeApp } from './c-home-app/c-home-app';
+import { Despesas } from './despesas/despesas';
+import { Financeiro } from './financeiro/financeiro';
+import { CondominioDados } from './condominio-dados/condominio-dados';
+import { CondominioDashboard } from './condominio-dashboard/condominio-dashboard';
+import { CondominioRelatorios } from './condominio-relatorios/condominio-relatorios';
+import { CondominioUnidades } from './condominio-unidades/condominio-unidades';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+    {path: '', component: CHomeApp},
+    {path: 'home', component: Home,},
+    {path: 'login', component:Login, title:"Login"},
+    {path: 'despesas', component:Despesas, title:"Despesas"},
+    {path: 'financeiro', component:Financeiro, title:"Financeiro"},
+    {path: 'condominio-dados', component:CondominioDados, title:"Dados do condomínio"},
+    {path: 'condominio-dashboard', component:CondominioDashboard, title:"Dashboard"},
+    {path: 'condominio-relatorios', component:CondominioRelatorios, title:"Relatórios"},
+    {path: 'condominio-unidades', component:CondominioUnidades, title:"Unidades"},
+    {path: 'pedido-login', component:PedidoDeLogin},
+    {path: 'receitas', component:Receitas, title:"Receitas"}
+
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { CSidebarApp } from './c-sidebar-app/c-sidebar-app';
+import { CHomeApp } from './c-home-app/c-home-app';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CSidebarApp, CHomeApp, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
